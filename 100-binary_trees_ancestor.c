068850098249c50f159ea4068b4124d8d0ac8755
hanifanwar380 <hanifanwar380@gmail.com>
2022-09-22 17:45:14 +0300

    binary trees

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
-binary_tree_t *recurse_for_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second);
+
+
 /**
  * binary_trees_ancestor - find lowest common ancestor
  * @first: first node to find ancestor for
@@ -9,48 +9,30 @@ binary_tree_t *recurse_for_ancestor(binary_tree_t *root,
  * Return: pointer to lowest common ancestor; NULL on failure
  * Authors: Hanif Miyanji & Maureen Mbugua
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+const binary_tree_t *second)
 {
-	binary_tree_t *root = NULL;
+	binary_tree_t *p, *q;
 
-	if (!first || !second)
+	if (first == NULL || second == NULL)
+	{
 		return (NULL);
+	}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
 
-	/* find root node */
-	root = (binary_tree_t *)first;
-	while (root->parent != NULL)
-		root = root->parent;
-
-	/* perform lowest common anscestor detection */
-	return (recurse_for_ancestor(root, first, second));
-}
-/**
- * recurse_for_ancestor - recursive component to find LCA
- * @root: pointer to root of tree
- * @first: pointer to one child
- * @second: pointer to other child
- *
- * Return: pointer to LCA
- */
-binary_tree_t *recurse_for_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second)
-{
-	binary_tree_t *leftLCA = NULL, *rightLCA = NULL;
-
-	if (root == NULL || first == NULL || second == NULL)
-		return (NULL);
-
-	if (root == first || root == second)
-		return (root);
-
-	leftLCA = recurse_for_ancestor(root->left, first, second);
-	rightLCA = recurse_for_ancestor(root->right, first, second);
-
-	if (leftLCA && rightLCA)
-		return (root);
-
-	if (leftLCA != NULL)
-		return (leftLCA);
-	return (rightLCA);
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
+	{
+		return (binary_trees_ancestor(first, q));
+	}
+	else if (q == NULL || p == second || (!q->parent && p))
+	{
+		return (binary_trees_ancestor(p, second));
+	}
+	return (binary_trees_ancestor(p, q));
 }
